c502e72131da113621001ddb097f7e549751f0ed
Kou Suzuki <40513273+kou72@users.noreply.github.com>
2022-12-18 02:00:43 +0900

    Add files via upload

---
+++ profile.ps1
@@ -0,0 +1,8 @@
+﻿
+Import-Module posh-git
+
+$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $false
+$GitPromptSettings.DefaultPromptPath.Text = '`n$(Get-PromptPath)'
+$GitPromptSettings.DefaultPromptPath.ForegroundColor = 'Orange'
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+$GitPromptSettings.DefaultPromptBeforeSuffix.ForegroundColor = [ConsoleColor]::Magenta
